dce8109006c54b70c13cbd3932852c3de3aba13e
Samala.Raghavi <20WH1A0553@BVRITHYDERABAD.EDU.IN>
2022-12-06 06:34:50 +0530

    Area_and_volume

---
+++ Area_and_volume.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include<cmath>
+using namespace std;
+int main(){
+	int r;
+	cout<<"Radius = ";
+	cin>>r;
+	cout<<"area of the circle = "<<M_PI * (r * r)<<"\n";
+	cout<<"volume of sphere = "<<M_PI * (r * r * r)<<"\n";
+}
