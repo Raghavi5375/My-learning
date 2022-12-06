b0c3119d4f53ac1711a8f6bff4d97d564bfc56b8
Samala.Raghavi <20WH1A0553@BVRITHYDERABAD.EDU.IN>
2022-12-06 06:36:12 +0530

    first_n_natural_numbers

---
+++ first_n_natural_numbers.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n, sum;
+	cout<<"n : ";
+	cin>>n;
+	sum = n * (n + 1)/2;
+	cout<<"sum of first "<<n<<" natural numbers is = "<<sum<<"\n";
+}
