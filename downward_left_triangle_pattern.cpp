c3c645c547e974b7bdfc6e3ddc6f40d585285b81
Samala.Raghavi <20WH1A0553@BVRITHYDERABAD.EDU.IN>
2022-12-14 14:14:26 +0530

    downward left triangle pattern

---
+++ downward_left_triangle_pattern.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"n : ";
+	cin>>n;
+	cout<<"\n";
+	for(int i=1; i<=n; i++)
+    {
+        for(int j=i; j<n; j++)
+        {
+            printf(" ");
+        }
+
+        for(int j=1; j<=i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+}
