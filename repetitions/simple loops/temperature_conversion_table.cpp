77149a9ccb0b98522a676ebb41a549ed9383b310
Samala.Raghavi <20WH1A0553@BVRITHYDERABAD.EDU.IN>
2022-12-19 15:19:49 +0530

    temperature conversion table

---
+++ repetitions/simple loops/temperature_conversion_table.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	cout<<"Celsius    Fahrenheit"<<"\n";
+	for(int i = 1 ; i <= 10 ; i++){
+		cout<<i * 10<<"             "<<((i * 10)*1.8) + 32<<"\n";
+	}
+}
